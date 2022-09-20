36f6ff1af96026376815c636e539d3d209d205ed
Gizem Izmir <gizkes1996@gmail.com>
2022-09-20 11:40:46 +0300

    App.js updated.

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+
+import { NativeBaseProvider } from "native-base";
+import { Provider } from "react-redux";
+
+import MainStackNavigation from "./src/navigation/mainStackNavigation";
+import { store } from "./src/store";
+import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <NativeBaseProvider>
+          <NavigationContainer>
+            <StatusBar style="auto" />
+            <MainStackNavigation />
+          </NavigationContainer>
+        </NativeBaseProvider>
+      </SafeAreaProvider>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
